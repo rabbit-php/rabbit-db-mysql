6d89ecc102773b30a42b41dd1e6f3368cfc66f74
Albert <amuluowin@163.com>
2021-06-25 09:21:09 +0800

    fix timeout to share

--- src/Connection.php
+++ src/Connection.php
@@ -51,7 +51,7 @@ class Connection extends \Rabbit\DB\Connection implements ConnectionInterface
         $this->username = $this->username ?? $username;
         $this->password = $this->password ?? $password;
         $timeout = $this->getPool()->getTimeout();
-        $this->share = $timeout ?? $timeout;
+        $this->share = $timeout > 0 ? (int)$timeout : $this->share;
         $dsn = "$driver:host=$host;port=$port;" . implode(';', $parts);
         $pdo = new $pdoClass($dsn, $this->username, $this->password, array_merge([
             PDO::ATTR_TIMEOUT => (int)$timeout,
