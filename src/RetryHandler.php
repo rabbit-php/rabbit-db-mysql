d0df5885ad00179441638a13578721d5e733400e
Albert <amuluowin@163.com>
2022-05-14 18:53:19 +0800

    opt retry

--- src/RetryHandler.php
+++ src/RetryHandler.php
@@ -50,7 +50,7 @@ class RetryHandler extends \Rabbit\DB\RetryHandler
     {
         if ($exception instanceof Exception) {
             $errorInfo = $exception->errorInfo;
-            if (($errorInfo[1] ?? false) && ((int)$errorInfo[1] === 70100 || (int)$errorInfo[1] === 2006)) {
+            if (($errorInfo[1] ?? false) && ((int)$errorInfo[1] === 70100 || (int)$errorInfo[1] === 2006 || (int)$errorInfo[1] === 2002)) {
                 return true;
             }
         }
